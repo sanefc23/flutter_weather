8dae04682a8bce865cc73991440b4f989f88c4bf
sanefc23 <san.efc@gmail.com>
2020-05-24 23:48:34 -0300

    complete OpenWeatherMap API fetch +  refactoring

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,32 +2,40 @@ import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 
+const kApiKey = '2ad0a4c620194b5cebca992498808d1f';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<void> getLocation() async {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  Future<void> getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude*lon=$longitude&appid=$kApiKey');
+    var weatherData = await networkHelper.getWeatherData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getWeatherData();
     return Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getLocation();
   }
 }
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,20 +1,18 @@
 import 'dart:convert';
-
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
 
-const kApiKey = '2ad0a4c620194b5cebca992498808d1f';
+class NetworkHelper {
+  NetworkHelper(this.url);
+  final String url;
 
-void getWeatherData() async {
-  Location location = Location();
-  await location.getCurrentLocation();
-  var url =
-      'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${kApiKey}';
-  http.Response response = await http.get(url);
-  if (response.statusCode == 200) {
-    String data = response.body;
-    var weatherDescription = jsonDecode(data);
-  } else {
-    print(response.statusCode);
+  Future getWeatherData() async {
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 }
